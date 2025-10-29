d5fbe7fbf27a96d5b8784714477b17c690b81404
Achref <boudhinaachrefm@gmail.com>
2025-10-29 16:49:10 +0100

    Matlab modified

--- matlab/linear_model_constants.m
+++ matlab/linear_model_constants.m
@@ -1,14 +1,14 @@
-mw = 1 ; %Masse d'une unit´e (roue + engrenage + rotor)
-mb = 1 ; %Masse du chassis du robot
-Ix = 1 ;Iy = 1 ;Iz = 1 ; %Moment d'inertie du chassis autour de chacun de ses axes, `a C
-Iwx = 1 ;Iwy = 1 ;Iwz = 1 ;%Moment d'inertie de la roue autour de chacun de ses axes, `a Cw
-Rho = 1 ; %position verticale(rayon du roue)
-l = 1 ; %Distance entre les deux roues
-d = 1 ; %Distance entre A et C
-di = 1 ; %Distance verticale entre l'IMU et l'axe des roues
-L = 1 ; %Inductance des moteurs 
-R = 1 ; %R´esistance des moteurs
-k = 1 ; %Constante de couple
+mw = 0 ; %Masse d'une unite (roue + engrenage + rotor)
+mb = 0.917 ; %Masse du chassis du robot
+Ix = 0 ;Iy = 0 ;Iz = 0 ; %Moment d'inertie du chassis autour de chacun de ses axes, a C
+Iwx = 0 ;Iwy = 0.00005 ;Iwz = 0 ;%Moment d'inertie de la roue autour de chacun de ses axes, a Cw
+Rho = 0.032 ; %position verticale(rayon du roue)
+l = 0.186 ; %Distance entre les deux roues
+d = 0.035 ; %Distance entre A et C
+di = 0 ; %Distance verticale entre l'IMU et l'axe des roues
+L = 0 ; %Inductance des moteurs 
+R = 2.2 ; %Resistance des moteurs
+k = 0.38 ; %Constante de couple
 g = 9.81; %constante de gravitation
 
 
@@ -22,3 +22,15 @@ E = Itoty+mb*d*d ; F = -mb*d; G = k;
 
 A1 = (F*A)/(E-D*B) ; A2 = (A*G-D*C)/(E-D*B) ; A3 = (-B*F)/(E-D*B);
 A4 = (-B*(A*G-D*C)-C*(E-D*B))/(A*(E-D*B)) ; A5 = (k*l)/(2*Rho);
+
+B1 = A2/(A2*A3-A1*A4) ; B2 = -A4/(A2*A3-A1*A4);
+
+AA = [0 , 1 , 0 , 0;
+          0 , 0 , 1 , 0;
+          0 , 0 , 0 , 1;
+          0 , -(2*A2*k)/(R*Rho*B1) , A1 , A2*(2*k/R+B2/B1)]
+    BB = [0 , 0;
+          0 , 0;
+          0 , 0;
+          A2/R , A2/R]
+    eig(AA)
