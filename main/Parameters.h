c3c3ca8f84904e1fa9a3e369910dc2376fd4b906
P J Anthony <104015990+pjanthony2001@users.noreply.github.com>
2025-11-03 13:25:03 +0100

    Update State

--- main/Parameters.h
+++ main/Parameters.h
@@ -95,6 +95,60 @@ constexpr CommMode commMode = CommMode::USB;
 // CONTROL PARAMETERS
 
 
+m_w = 0.41 ; %Masse d'une unite (roue + engrenage + rotor)
+m_b = 1.8 ; %Masse du chassis du robot
+
+I_x = 9.942 * 1e-3;
+I_y = 5.31 * 1e-3;
+I_z = 5.953 * 1e-3; %Moment d'inertie du chassis autour de chacun de ses axes, a C
+
+Iw_x = 4.8886 * 1e-4;
+Iw_y = 2.5233 * 1e-4;
+Iw_z = 2.5307 * 1e-4; %Moment d'inertie de la roue autour de chacun de ses axes, a Cw
+
+l = 0.171 ; %Distance entre les deux roues
+d = 0.044 ; %Distance entre A et C
+rho = 0.050 ; %position verticale(rayon du roue)
+d_i = 0.100 ; %Distance verticale entre l'IMU et l'axe des roues
+
+L = 0 ; %Inductance des moteurs 
+R = 2.181818 ; %Resistance des moteurs
+k = 0.3346 ; %Constante de couple
+
+g = 9.81; %constante de gravitation
+
+
+M = m_b + 2 * m_w;
+I_psi = I_x + I_z + Iw_x + Iw_z + (m_w * l * l) / 2 + (Iw_y * l * l) / (2 * rho * rho);
+Itot_y = I_y + 2 * Iw_y;
+
+A_0 = M + (2 * Iw_y) / (rho * rho);
+A_1 = m_b * d;
+A_2 = -k / rho;
+A_3 = m_b * d;
+A_4 = Itot_y + m_b * d * d;
+A_5 = -m_b * d * g;
+A_6 = k;
+
+
+B_0  = -2 * k / (rho * R);
+B_1 = 2 * k / R;
+B_2 = 1 / R;
+
+
+A = inv(T) * Q_0;
+B = inv(T) * Q_1;
+ 
+A_W = [0 1 0
+       0 0 g
+       0 0 0];
+
+B_W = [-d_i 0
+        0 1
+        1 0];
+
+
+C_W = [2/rho 0 -2];
 
 
 
